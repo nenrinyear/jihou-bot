2b8e3feaad224158977b65db729ed8ae599b40e7
年輪 <nenrin.yearring@gmail.com>
2024-09-25 12:14:20 +0900

    ⏫️Update package version and remove old messages

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,4 +1,4 @@
-import { ActivityType, Client, ClientEvents } from "discord.js";
+import { ActivityType, Client, ClientEvents, FetchMessagesOptions } from "discord.js";
 import registerCommands from "../register";
 import path from "path";
 
@@ -14,11 +14,23 @@ const handler = async (client: Client<true>) => {
     const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_ID ?? '')
     if (!channel?.isTextBased()) return;
 
+    channel.messages.fetch({
+        limit: 100,
+    } as FetchMessagesOptions).then(messages => {
+        messages.forEach(message => {
+            if (message.author.id === client.user?.id) {
+                message.delete();
+            }
+        });
+    }).catch(console.error);
+
     const now = new Date();
     // 0秒もしくは30秒になるまで待機
     const delay = 30 - now.getSeconds() % 30;
     console.log(`⏳${delay}秒待機します`);
     await new Promise((resolve) => setTimeout(resolve, delay * 1000));
+
+    let sent_message_id = '';
 
     // 30秒ごとに時報を表示
     setInterval(async() => {
@@ -34,16 +46,23 @@ const handler = async (client: Client<true>) => {
         ) {
             const message = `🕒時報BOTが${hour}時${minute}分${second}秒をお知らせします。`;
             console.log(message);
-            
+
+            if (sent_message_id) {
+                const sent_message = await channel.messages.fetch(sent_message_id);
+                sent_message.delete();
+            }
+
             if (hour === 0) {
-                await channel.send({
+                const sent_message = await channel.send({
                     content: message,
                     files: [
                         path.join(__dirname, '../', 'nico.mp4'),
                     ],
                 });
+                sent_message_id = sent_message.id;
             } else {
-                await channel.send(message);
+                const sent_message = await channel.send(message);
+                sent_message_id = sent_message.id;
             }
         }
     }, 1000);
